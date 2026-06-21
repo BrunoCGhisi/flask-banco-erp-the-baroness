fa36d511fefa0e756df37353d676801892a6eafa
brunocghisi <brunocostaghisi@gmail.com>
2026-06-21 11:12:23 -0300

    quartos listou

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from routes.quartos import quartos_bp
-from routes.hospedes import hospedes_bp
+from routes.hospedes import hospede_bp
 from routes.reservas import reservas_bp
 from routes.pagamentos import pagamentos_bp
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(quartos_bp)
-app.register_blueprint(hospedes_bp)
+app.register_blueprint(hospede_bp)
 app.register_blueprint(reservas_bp)
 app.register_blueprint(pagamentos_bp)
 
--- routes/hospedes.py
+++ routes/hospedes.py
@@ -8,7 +8,6 @@ hospede_bp = Blueprint(
     url_prefix="/hospedes"
 )
 
-
 @hospede_bp.route("/", methods=["GET"])
 def listar():
 
