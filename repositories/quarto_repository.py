a3cf20363f75cf69655e3da3a072d206351a4fff
brunocghisi <brunocostaghisi@gmail.com>
2026-06-22 12:39:07 -0300

    quartos working edit

--- repositories/quarto_repository.py
+++ repositories/quarto_repository.py
@@ -102,7 +102,6 @@ def atualizar_quarto(id_quarto, dados):
                            status = %s,
                            id_categoria = %s
                        WHERE id_quarto = %s
-                           RETURNING *
                        """,
                        (
                            dados["numero"],
@@ -112,17 +111,30 @@ def atualizar_quarto(id_quarto, dados):
                            id_quarto
                        ))
 
-        quarto = cursor.fetchone()
-
         conn.commit()
 
-        return quarto
+        cursor.execute("""
+                       SELECT
+                           q.id_quarto,
+                           q.numero,
+                           q.andar,
+                           q.status,
+                           q.id_categoria,
+                           c.nome AS categoria
+                       FROM quarto q
+                       JOIN categoria_quarto c
+                           ON c.id_categoria = q.id_categoria
+                       WHERE q.id_quarto = %s
+                       """,
+                       (id_quarto,)
+                       )
+
+        return cursor.fetchone()
 
     finally:
 
         cursor.close()
         conn.close()
-
 
 def deletar_quarto(id_quarto):
 
