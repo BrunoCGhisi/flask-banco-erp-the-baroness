839fb4bc1d6443068ea2fba3e5a276efee0d7bb8
brunocghisi <brunocostaghisi@gmail.com>
2026-06-21 21:01:23 -0300

    quartos working

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ from flask_cors import CORS
 
 from routes.hospedes import hospede_bp
 from routes.categorias import categoria_bp
+from routes.quartos import quarto_bp
 
 app = Flask(__name__)
 
 CORS(app)
 
-app.register_blueprint(
-    hospede_bp
-)
+app.register_blueprint(hospede_bp)
+app.register_blueprint(categoria_bp)
+app.register_blueprint(quarto_bp)
 
-app.register_blueprint(
-    categoria_bp
-)
 
 @app.route("/")
 def home():
--- routes/quartos.py
+++ routes/quartos.py
@@ -1,13 +1,112 @@
 from flask import Blueprint
+from flask import request
+from flask import jsonify
 
-quartos_bp = Blueprint(
-    "quartos",
+from services import quarto_service
+
+
+quarto_bp = Blueprint(
+    "quarto",
     __name__,
     url_prefix="/quartos"
 )
 
-@quartos_bp.route("/", methods=["GET"])
-def listar_quartos():
-    return {
-        "mensagem": "Lista de quartos"
-    }
+
+@quarto_bp.route("/", methods=["GET"])
+def listar():
+
+    try:
+
+        return jsonify(
+            quarto_service.listar()
+        )
+
+    except Exception as e:
+
+        return jsonify({
+            "erro": str(e)
+        }), 500
+
+
+@quarto_bp.route("/<int:id_quarto>", methods=["GET"])
+def buscar(id_quarto):
+
+    try:
+
+        return jsonify(
+            quarto_service.buscar_por_id(
+                id_quarto
+            )
+        )
+
+    except Exception as e:
+
+        return jsonify({
+            "erro": str(e)
+        }), 500
+
+
+@quarto_bp.route("/", methods=["POST"])
+def criar():
+
+    try:
+
+        dados = request.json
+
+        quarto = quarto_service.criar(
+            dados
+        )
+
+        return jsonify(
+            quarto
+        ), 201
+
+    except Exception as e:
+
+        return jsonify({
+            "erro": str(e)
+        }), 400
+
+
+@quarto_bp.route("/<int:id_quarto>", methods=["PUT"])
+def atualizar(id_quarto):
+
+    try:
+
+        dados = request.json
+
+        quarto = quarto_service.atualizar(
+            id_quarto,
+            dados
+        )
+
+        return jsonify(
+            quarto
+        )
+
+    except Exception as e:
+
+        return jsonify({
+            "erro": str(e)
+        }), 400
+
+
+@quarto_bp.route("/<int:id_quarto>", methods=["DELETE"])
+def deletar(id_quarto):
+
+    try:
+
+        quarto_service.deletar(
+            id_quarto
+        )
+
+        return jsonify({
+            "mensagem":
+                "Quarto removido com sucesso"
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "erro": str(e)
+        }), 400
